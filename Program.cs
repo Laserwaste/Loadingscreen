3494a26cb27840150eb81aecda34314e21f912d5
Laserwaste <permataalkautsar1@gmail.com>
2026-09-19 07:13:04 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int totalstep = 5;
+
+for (int step = 1; step <= totalstep ; step++)
+
+{
+    Console.WriteLine($"Loading.. Step {step} of {totalstep} ");    
+}
+Console.WriteLine("\nDungeon is ready, Enter Now!");
